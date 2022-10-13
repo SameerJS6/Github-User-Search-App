d9310f3112ff6101d2f491112a5646f0a94ad1fa
Sameer Singh <sameersingh1234pok@gmail.com>
2022-10-13 22:48:03 +0530

    temp theme toggle

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const body = document.getElementById('body');
-const ThemeButton = document.querySelector('.theme');
+const ThemeButton = document.querySelector('.theme-select');
+const DarkButton = document.querySelector('.theme');
+const lightButton = document.querySelector('.light');
+const Sun = document.querySelector('.sun');
+const Moon = document.querySelector('.moon');
 
 
 ThemeButton.addEventListener('click', ()=> {
+    // For Temporialy 
+    DarkButton.classList.toggle('none')
+    Moon.classList.toggle('none')
+    lightButton.classList.toggle('none')
+    Sun.classList.toggle('none')
+
     body.classList.toggle('DarkMode');
 })
