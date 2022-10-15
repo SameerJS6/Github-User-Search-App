3abf2cee5c7d8a4e1aa4123269935d5da792b4ae
Sameer Singh <sameersingh1234pok@gmail.com>
2022-10-15 11:12:34 +0530

    Minor improvement here

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ const Twitter = document.querySelector('[data-twitter]');
 const Website = document.querySelector('[data-website]');
 const Company = document.querySelector('[data-company]');
 
+const Months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "JUn",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
 // This function is for only removing whtie space in between name from input 
 function InputName() {
     const InputName = Input.value;
@@ -27,39 +42,55 @@ function api() {
     .then((result) => result.json())
     .then((data)=> {
         console.log(data);
-        AvatarImg.forEach((avatar) => {
-            avatar.src = `${data.avatar_url}`;
-        });
-        UserName.innerHTML = `${data.name}`;
-        UserLink.innerHTML = `@${data.login}`;
-        UserLink.href = `${data.html_url}`;
-        Bio.innerHTML = `${data.bio}`;
-        if(Bio.innerHTML == 'null') {
-            Bio.innerHTML = 'Not Available'
+        UpdateProfile(data);
+        if(data.message == 'Not Found') {
+            NoResult.classList.remove('.none')
         }
-        Repo.innerHTML = `${data.public_repos}`;
-        Followers.innerHTML = `${data.followers}`;
-        Following.innerHTML = `${data.following}`;
-        LocaTion.innerHTML = `${data.location}`;
-        if(LocaTion.innerHTML == 'null') {
-            LocaTion.innerHTML = 'Not Available'
-        }
-        Twitter.innerHTML = `${data.twitter_username}`;
-        Twitter.href = `https://twitter.com/${data.twitter_username}`;
-        if(Twitter.innerHTML == 'null') {
-            Twitter.innerHTML = 'Not Available'
-        }
-        Website.innerHTML = `${data.blog}`;
-        Website.href = `${data.blog}`;
-        if(Website.innerHTML == '') {
-            Website.innerHTML = 'Not Available'
-        }
-        Company.innerHTML = `${data.company}`;
-        if(Company.innerHTML == 'null') {
-            Company.innerHTML = 'Not Available'
-        }
-        DateofJoin.innerHTML = `${data.created_at}`;
     })
+    .catch((error) => {
+        throw error;
+    })
+}
+
+// Updating the Profile function 
+function UpdateProfile(data) {
+    AvatarImg.forEach((avatar) => {
+        avatar.src = `${data.avatar_url}`;
+    });
+    UserName.innerHTML = `${data.name}`;
+    if(data.name == null) {
+        UserName.innerHTML = `${data.login}`;
+    }
+    UserLink.innerHTML = `@${data.login}`;
+    UserLink.href = `${data.html_url}`;
+    Bio.innerHTML = `${data.bio}`;
+    if(Bio.innerHTML == 'null') {
+        Bio.innerHTML = 'This proile has no bio';
+    }
+    Repo.innerHTML = `${data.public_repos}`;
+    Followers.innerHTML = `${data.followers}`;
+    Following.innerHTML = `${data.following}`;
+    LocaTion.innerHTML = `${data.location}`;
+    if(LocaTion.innerHTML == 'null') {
+        LocaTion.innerHTML = 'Not Available'
+    }
+    Twitter.innerHTML = `${data.twitter_username}`;
+    Twitter.href = `https://twitter.com/${data.twitter_username}`;
+    if(Twitter.innerHTML == 'null') {
+        Twitter.innerHTML = 'Not Available'
+    }
+    Website.innerHTML = `${data.blog}`;
+    Website.href = `${data.blog}`;
+    if(Website.innerHTML == '') {
+        Website.innerHTML = 'Not Available'
+    }
+    Company.innerHTML = `${data.company}`;
+    if(Company.innerHTML == 'null') {
+        Company.innerHTML = 'Not Available'
+    }
+    let datasegment = data.created_at.split('T').shift().split('-')
+    console.log(datasegment)
+    DateofJoin.innerHTML = `Joined ${datasegment[2]} ${datasegment[1]} ${datasegment[0]}`;
 }
 
 // Event Listener for Submit button and call back for api function above 
