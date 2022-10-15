1edb2ed396a77f2ce99cc71abaa60d3a810e971e
Sameer Singh <sameersingh1234pok@gmail.com>
2022-10-15 16:58:22 +0530

    All the basic functinality done

--- script.js
+++ script.js
@@ -33,9 +33,6 @@ const Months = [
 function InputName() {
     const Inputkey = Input.value;
     const SearchName = Inputkey.split(' ').join('')
-    if(SearchName == '') {
-        return 
-    }
     return SearchName;
 }
 
@@ -47,11 +44,6 @@ function api() {
         console.log(data);
         console.log(NoResult.innerText)
         UpdateProfile(data);
-        
-        if(data.name == null) {
-            UserName.innerHTML = `${data.login}`;
-            // NoResult.style.display = 'none'
-        }
     })
     .catch((error) => {
         throw error;
@@ -85,44 +77,64 @@ function checknull (param1, param2) {
 }
 // Updating the Profile function 
 function UpdateProfile(data) {
-    AvatarImg.forEach((avatar) => {
-        avatar.src = `${data.avatar_url}`;
-    });
-    UserName.innerHTML = `${data.name}`;
-    UserLink.innerHTML = `@${data.login}`;
-    UserLink.href = `${data.html_url}`;
-    Bio.innerHTML = `${data.bio}`;
-    if(Bio.innerHTML == 'null') {
-        Bio.innerHTML = 'This proile has no bio';
+    if(data.message !== 'Not Found') {
+        // NoResult.style.display = 'none';
+        // NoResult.style.visibility = 'hidden'
+        // NoResult.style.opacity = '0';
+        // NoResult.style.right = '-5%';
+        // NoResult.setAttribute('data-hidden', true);
+        NoResult.removeAttribute('data-hidden')
+        
+
+        AvatarImg.forEach((avatar) => {
+            avatar.src = `${data.avatar_url}`;
+        });
+        UserName.innerHTML = `${data.name}`;
+        UserLink.innerHTML = `@${data.login}`;
+        UserLink.href = `${data.html_url}`;
+        if(data.name == null) {
+            UserName.innerHTML = `${data.login}`;
+        }
+        Bio.innerHTML = `${data.bio}`;
+        if(Bio.innerHTML == 'null') {
+            Bio.innerHTML = 'This proile has no bio';
+        }
+        Repo.innerHTML = `${data.public_repos}`;
+        Followers.innerHTML = `${data.followers}`;
+        Following.innerHTML = `${data.following}`;
+        LocaTion.innerHTML = checknull(data.location, LocaTion);
+        // if(LocaTion.innerHTML == 'null') {
+        //     LocaTion.innerHTML = 'Not Available'
+        // }
+        Twitter.innerHTML = checknull(data.twitter_username, Twitter);
+        Twitter.href = `https://twitter.com/${data.twitter_username}`;
+        // if(Twitter.innerHTML == 'null') {
+        //     Twitter.innerHTML = 'Not Available'
+        // }
+        Website.innerHTML = checknull(data.blog, Website);
+        Website.href = `${data.blog}`;
+        // if(Website.innerHTML == '') {
+        //     Website.innerHTML = 'Not Available'
+        // }
+        Company.innerHTML = checknull(data.company, Company);
+        // if(Company.innerHTML == 'null') {
+        //     Company.innerHTML = 'Not Available'
+        // }
+        let datasegment = data.created_at.split('T').shift().split('-');
+        DateofJoin.innerHTML = `Joined ${datasegment[2]} ${Months[datasegment[1]-1]} ${datasegment[0]}`;
+    } else {
+        // NoResult.style.display = 'block';
+        // NoResult.style.visibility = 'visible'
+        // NoResult.style.opacity = '1'
+        // NoResult.style.right = '26%';
+        NoResult.setAttribute('data-hidden', true);
     }
-    Repo.innerHTML = `${data.public_repos}`;
-    Followers.innerHTML = `${data.followers}`;
-    Following.innerHTML = `${data.following}`;
-    LocaTion.innerHTML = checknull(data.location, LocaTion);
-    // if(LocaTion.innerHTML == 'null') {
-    //     LocaTion.innerHTML = 'Not Available'
-    // }
-    Twitter.innerHTML = checknull(data.twitter_username, Twitter);
-    Twitter.href = `https://twitter.com/${data.twitter_username}`;
-    // if(Twitter.innerHTML == 'null') {
-    //     Twitter.innerHTML = 'Not Available'
-    // }
-    Website.innerHTML = checknull(data.blog, Website);
-    Website.href = `${data.blog}`;
-    // if(Website.innerHTML == '') {
-    //     Website.innerHTML = 'Not Available'
-    // }
-    Company.innerHTML = checknull(data.company, Company);
-    // if(Company.innerHTML == 'null') {
-    //     Company.innerHTML = 'Not Available'
-    // }
-    let datasegment = data.created_at.split('T').shift().split('-')
-    console.log(datasegment)
-    DateofJoin.innerHTML = `Joined ${datasegment[2]} ${datasegment[1]} ${datasegment[0]}`;
+   
 }
 
 // Event Listener for Submit button and call back for api function above 
 SubmitBtn.addEventListener('click', ()=> {
+    if(Input.value == '') return
     api();
 })
 
